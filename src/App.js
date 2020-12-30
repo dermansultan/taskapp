be02c7b56f425a7880a432f5aeb244723fe7dae6
dermansultan <abdurrehmansultan4@gmail.com>
2020-12-30 13:36:46 -0500

    Finished

--- src/App.js
+++ src/App.js
@@ -1,11 +1,52 @@
-
+import React from "react";
+import { OverviewList } from "./components/Overview";
 // Will handle input field logic
 
-function App() {
-  return (
-    <div className="App">
-    </div>
-  );
+// Scope of app:
+// input, submit button
+// add content to tasks array that is managed in state
+// for each tasks array html list elem should be rendered.
+// Overview will be a component that renders out the list.
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    //state
+    this.state = {
+      value: "",
+      tasks: [],
+    };
+
+    //binded methods
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({ value: event.target.value });
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    this.setState(prevState => ({
+      tasks: [...prevState.tasks, prevState.value]
+    }))
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <form onSubmit={this.handleSubmit}>
+          <label>Task:</label>
+          <br></br>
+          <input type="text" value={this.state.value} onChange={this.handleChange} />
+          <input type="submit" value="Submit"/>
+        </form>
+        <OverviewList tasks={this.state.tasks}></OverviewList>
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/components/Overview.js
+++ src/components/Overview.js
@@ -1,3 +1,7 @@
-import React from "react"
+import React from "react";
 
 // Will render out just tasks
+export function OverviewList(props) {
+  const listItems = props.tasks.map((task) => <li key={task.toString()}>{task}</li>);
+  return <ol className="list Container">{listItems}</ol>;
+}
